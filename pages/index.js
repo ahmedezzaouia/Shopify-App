c0f57916427d24ad89cf18a1fc373dce8ea7d8c0
ahmedezzaouia <35857662+ahmedezzaouia@users.noreply.github.com>
2022-02-06 12:13:31 +0100

    productCard

--- pages/index.js
+++ pages/index.js
@@ -19,14 +19,23 @@ const Index = () => (
     <div className="productsList">
       <div className="productCard">
         <div className="productCard_image">
-          <img src="" alt="" />
-          image
+          <img
+            src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHw%3D&w=1000&q=80"
+            height="100"
+            alt=""
+          />
         </div>
         <div className="product_content">
           <h5>Product title</h5>
-          <p>Product Description....</p>
+          <p>
+            In publishing and graphic design, Lorem ipsum is a placeholder text
+            commonly used to demonstrate the visual form of a document or a
+            typeface without relying on meaningful content. Lorem ipsum may be
+            used as a placeholder before the final copy is available. Wikipedia
+            .
+          </p>
         </div>
-        <div className="product_price">15.05$</div>
+        <div className="product_price">15 </div>
       </div>
     </div>
   </Page>
